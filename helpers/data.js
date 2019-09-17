79ccfb4039ee073819a6e0d5a0f787821635c8de
ImanBekkaye <imanbekkaye@hotmail.com>
2019-09-17 18:33:17 +0200

    dist alg

--- helpers/data.js
+++ helpers/data.js
@@ -1,7 +1,6 @@
 const db = require('../models/models');
 let geodist = require('geodist');
 
-
 var getAllCustomers = function(regFunction){
 
     db.Customers.findAll().then(rows => {
@@ -13,13 +12,50 @@ var getAllCustomers = function(regFunction){
                 weigth: row.dataValues.weight,
                 volume: row.dataValues.volume,
                 longitude: row.dataValues.longitude,
-                latitude: row.dataValues.latitude}
+                latitude: row.dataValues.latitude,
+                posjecen: 0
+            };
             allCustomers.push(customer);
         });
-        allCustomers.sort(( a,b )=>{
-            return geodist({lat: a.latitude, lon: a.longitude}, {lat: b.latitude, lon: b.longitude})
-        })
-        regFunction(allCustomers);
+
+        let sortirani_niz = [];
+        let min = Math.ceil(0);
+        let max = Math.floor(allCustomers.length);
+        let random_index =  Math.floor(Math.random() * (max - min)) + min;
+        console.log(random_index)
+        if(allCustomers.length !== 0) {
+            if (allCustomers.length === 1){
+                random_index = 0;
+            }
+            allCustomers[random_index].posjecen = 1;
+            let trenutni = random_index;
+            let udaljenost = 0;
+            sortirani_niz.push(allCustomers[trenutni]);
+
+            while(sortirani_niz.length !== allCustomers.length){
+                let min_udaljenost = 10000000000;
+                let min_index = -1;
+                for(let j = 0; j<allCustomers.length; j++){
+                    if(allCustomers[j].posjecen === 0){
+                        udaljenost = geodist({lat: allCustomers[j].latitude, lon: allCustomers[j].longitude},
+                            {lat: allCustomers[trenutni].latitude, lon: allCustomers[trenutni].longitude});
+                        if (udaljenost <= min_udaljenost){
+                            min_udaljenost = udaljenost;
+                            min_index = j;
+                        }
+                    }
+                }
+                if (min_index === -1) {
+                    break;
+                }
+                trenutni = min_index;
+                allCustomers[min_index].posjecen = 1;
+                sortirani_niz.push(allCustomers[min_index])
+
+            }
+        }
+
+        regFunction(sortirani_niz);
 
     }).catch(err => {
         console.log('error neki',err.message);
